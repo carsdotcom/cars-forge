b83ce7969bc4f3d6da65aaa02f94e1dd60bd8bf8
Joao Moreira <joao.ag.moreira@gmail.com>
2022-09-27 13:11:26 -0400

    Update release action (#1)
    * Run github action only when project version is updated.
    
    * Use bump2version to manage version.
    
    * Add step in workflow to create GitHub release after uploading to PYPI.
    
    * Bump version: 1.0.0 → 1.0.1b1
    
    * Do not release beta versions on github.
    
    * Fix version.
    
    * Update python-publish.yml
    
    * Update python-publish.yml

---
+++ .github/scripts/generate_rn.py
@@ -0,0 +1,28 @@
+"""Extract a section of the changelog specific to a given version."""
+import configparser
+
+config = configparser.ConfigParser()
+with open(".bumpversion.cfg", "r", encoding="utf-8") as fobj:
+    config.read_file(fobj)
+    version = config['bumpversion']['current_version']
+
+rn = f"""
+```
+pip install cars-forge=={version}
+```
+
+---"""
+with open("CHANGELOG.md", "r", encoding="utf-8") as changelog:
+    is_read = False
+    for line in changelog:
+        # Starting current version section of changelog
+        if version in line:
+            is_read = True
+        # Done with current version
+        elif line.startswith("## [") and version not in line:
+            is_read = False
+        elif is_read and (line != ""):
+            rn += line
+
+with open("release_notes.md", "w", encoding="utf-8") as release_notes:
+    release_notes.write(rn)
